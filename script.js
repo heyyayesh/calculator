a85a9c3a9abc45bd0584ff06fff853f352b78290
heyyayesh <biswalayesh3@gmail.com>
2021-11-04 20:16:43 +0530

    add keyboard support and refactor code

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Selects and stores all html elements.
+
 const numbersBtn = document.querySelectorAll('.number');
 const operatorsBtn = document.querySelectorAll('.operator');
 const equalsBtn = document.querySelector('#equals');
@@ -7,9 +9,13 @@ const deleteBtn = document.querySelector('#delete');
 const outputDisplay = document.querySelector('.output');
 const historyDisplay = document.querySelector('.history');
 
+// Declares some variables to be used later.
+
 let operand1 = '', operand2 = '';
 let operation = null;
 isSecondOperand = false;
+
+// Adds functionality to all number buttons.
 
 numbersBtn.forEach((numberBtn) => {
     numberBtn.addEventListener('click', appendNumber);
@@ -28,23 +34,7 @@ function appendNumber(e){
     else outputDisplay.textContent += e.target.textContent;
 }
 
-function clearDisplay(){
-    outputDisplay.textContent = '0';
-    historyDisplay.textContent = '0';
-    isSecondOperand = false;
-    operand1 = '';
-    operand2 = '';
-    operation = null; 
-}
-
-function setOperand(){
-    if(!isSecondOperand){
-        operand1 = outputDisplay.textContent;
-    }
-    else if(isSecondOperand){
-        operand2 = outputDisplay.textContent;
-    }
-}
+// Adds functionality to all operator buttons.
 
 operatorsBtn.forEach((operatorBtn) => {
     operatorBtn.addEventListener('click', (e) => {
@@ -56,6 +46,63 @@ operatorsBtn.forEach((operatorBtn) => {
         operation = e.target.textContent;
     })
 })
+
+// Adds keyboard support to all numbers and operations.
+
+window.addEventListener('keydown', appendKey);
+
+function appendKey(e){
+    console.log(e.key);
+    if(e.key == '0' || e.key == '1' || e.key == '2' || e.key == '3' || e.key == '4' || e.key == '5' || e.key == '6' || e.key == '7' || e.key == '8' || e.key == '9' || e.key == '.'){
+        if(checkPoint(outputDisplay.textContent) && e.key == '.') return;
+        if(outputDisplay.textContent == '0'){
+            if(e.key == '.'){
+                outputDisplay.textContent += e.key;
+            }
+            else{
+                outputDisplay.textContent = e.key;
+            }
+        }
+        
+        else outputDisplay.textContent += e.key;
+    }
+    else if(e.key == '+' || e.key == '-' || e.key == '^'){
+        setOperand();   
+        historyDisplay.textContent = evaluate(operand1, operand2, operation) + ' ' + e.key;
+        operand1 = evaluate(operand1, operand2, operation);
+        outputDisplay.textContent = '0';
+        isSecondOperand = true;
+        operation = e.key;
+    }
+    else if(e.key == '*'){
+        setOperand();   
+        historyDisplay.textContent = evaluate(operand1, operand2, operation) + ' ' + '×';
+        operand1 = evaluate(operand1, operand2, operation);
+        outputDisplay.textContent = '0';
+        isSecondOperand = true;
+        operation = '×';
+    }
+    else if(e.key == '/'){
+        setOperand();   
+        historyDisplay.textContent = evaluate(operand1, operand2, operation) + ' ' + '÷';
+        operand1 = evaluate(operand1, operand2, operation);
+        outputDisplay.textContent = '0';
+        isSecondOperand = true;
+        operation = '÷';
+    }
+    else if(e.key == '=' || e.key == 'Enter'){
+        equals();
+    }
+    else if(e.key == 'c' || e.key == 'C'){
+        clearDisplay();
+    }
+    else if(e.key == 'Backspace'){
+        del();
+    }
+    else return;
+}
+
+// This function calls the appropriate function depending on the operator passed to it.
 
 function evaluate(op1, op2, opn){
     if(op2 == '' || opn == null) return op1;
@@ -75,30 +122,7 @@ function evaluate(op1, op2, opn){
     }
 }
 
-function maxPrecision(a, b){
-    let a_arr = a.toString().split('.');
-    let b_arr = b.toString().split('.');
-
-    let decimal_a, decimal_b;
-
-    if(a_arr.length == 1){
-        decimal_a = 0;
-    }
-    else{
-        let c = a_arr[1].toString().split('');
-        decimal_a = c.length;
-    }
-
-    if(b_arr.length == 1){
-        decimal_b = 0;
-    }
-    else{
-        let d = b_arr[1].toString().split('');
-        decimal_b = d.length;
-    }
-
-    return decimal_a > decimal_b ? decimal_a : decimal_b;
-}
+// Defines the add, subtract, multiply, divide and exponent function.
 
 function add(a, b){
     let c = parseFloat(a) + parseFloat(b)
@@ -144,6 +168,10 @@ function exponent(a, b){
     else return c;
 }
 
+// Adds functionality to equals button.
+
+equalsBtn.addEventListener('click', equals);
+
 function equals(){
     setOperand();
     historyDisplay.textContent = `${operand1} ${operation} ${operand2}`;
@@ -154,9 +182,20 @@ function equals(){
     operation = null; 
 }
 
-equalsBtn.addEventListener('click', equals);
+// Adds functionality to C button.
 
 clearBtn.addEventListener('click', clearDisplay);
+
+function clearDisplay(){
+    outputDisplay.textContent = '0';
+    historyDisplay.textContent = '0';
+    isSecondOperand = false;
+    operand1 = '';
+    operand2 = '';
+    operation = null; 
+}
+
+// Adds functionality to Del button.
 
 deleteBtn.addEventListener('click', del);
 
@@ -164,6 +203,42 @@ function del(){
     let str = outputDisplay.textContent.toString().slice(0, -1);
     if(str === '') outputDisplay.textContent = '0';
     else outputDisplay.textContent = str;
+}
+
+// Helper functions.
+
+function setOperand(){
+    if(!isSecondOperand){
+        operand1 = outputDisplay.textContent;
+    }
+    else if(isSecondOperand){
+        operand2 = outputDisplay.textContent;
+    }
+}
+
+function maxPrecision(a, b){
+    let a_arr = a.toString().split('.');
+    let b_arr = b.toString().split('.');
+
+    let decimal_a, decimal_b;
+
+    if(a_arr.length == 1){
+        decimal_a = 0;
+    }
+    else{
+        let c = a_arr[1].toString().split('');
+        decimal_a = c.length;
+    }
+
+    if(b_arr.length == 1){
+        decimal_b = 0;
+    }
+    else{
+        let d = b_arr[1].toString().split('');
+        decimal_b = d.length;
+    }
+
+    return decimal_a > decimal_b ? decimal_a : decimal_b;
 }
 
 function checkPoint(str){
